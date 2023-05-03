a7d139eb576c1da38f29765e8b03014cb98014a2
h3ma111 <65867037+h3ma111@users.noreply.github.com>
2023-05-04 07:23:14 +0800

    Update Utils.java

--- src/main/java/com/digital/electronics/utils/Utils.java
+++ src/main/java/com/digital/electronics/utils/Utils.java
@@ -1,11 +1,19 @@
 package com.digital.electronics.utils;
 
+import com.digital.electronics.model.Product;
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class Utils {
     public static float round(float d, int decimalPlace) {
         BigDecimal bd = new BigDecimal(Float.toString(d));
         bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
         return bd.floatValue();
+    }
+    public static double calculateBuyOneGetHalfPrice(Product product, int quantity) {
+        BigDecimal divide = new BigDecimal(quantity / 2)
+                .divide(new BigDecimal(2), new MathContext(2, RoundingMode.HALF_UP));
+        return product.price * divide.doubleValue();
     }
 }
